495022d4d2c946258b979a01551e01141c23d680
Alejandro Ortiz <janoflute@gmail.com>
2025-08-21 00:48:26 -0400

    ApiRest creada en UserRestController

--- src/main/java/com/example/orvar/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/example/orvar/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,5 +1,22 @@
 package com.example.orvar.curso.springboot.webapp.springboot_web.controllers;
 
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserRestController {
 
+    @GetMapping("/details2")
+    public Map<String, Object> details(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("title", "Hola Mundo Spring Boot");
+        body.put("name", "Orvar");
+        body.put("lastName", "Oddr");
+        return body;
+    }
 }
